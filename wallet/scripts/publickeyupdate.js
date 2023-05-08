f5bc5ad7b41019bef8c1711f4e4f94c026e62161
KaiworChen <1021516291@qq.com>
2023-05-08 16:16:08 +0800

    update

--- wallet/scripts/publickeyupdate.js
+++ wallet/scripts/publickeyupdate.js
@@ -41,23 +41,5 @@ const publicKeyToComponents = publicKey => {
  const {domain, selector,exponent,modulus} = getPublicKey({domain:'gmail.com',selector:'20221208'})
  .then (publicKey=>{publicKeyToComponents(publicKey)});
 
- //const{exponent,modulus} = publicKeyToComponents(publicKey);
  await publicPKeyOrcle.setPublicKey(domain,selector,exponent,modulus);
  
-
-
-//publicKeyToComponents(password);
-
-// const password = new NodeRSA('-----BEGIN PUBLIC KEY-----'+
-//   'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs6VD4JQQHRZ2EZNR50TakIQFY3GGP4VfY3BPbueQe6Xnwvhuh5wkVllU8XTIxxZt+T6ug7tb6QbACupQ9I/TA2dKkiWv4WXUt+qRfkcZvzedEgGJalQLvPR2iZaImqWiMNXOintifm0g88rsfRrYUWa3T+YIo5biIg9Anqmo9wbKDmtMlWMgkoUqKHbjDVgHb6iqYjDV/HEkp5htBCacP5KvdcH18m6P2qUncfxfPj5DkrwyPAk3x4Mp6sm4wqABYyyRukxO2/lIKUjZgbHAEudRG9lZqliykMOA5ZAKQzD0ElWQiYPAK2s95+G0FHd4JWtD5pUqqzuY+hOnm/0dDwIDAQAB'+
-//   '-----END PUBLIC KEY-----')
-// console.log(password.exportKey("components-public"))
-
-// console.log(publicKeyToComponents(publicKey))
-
-//const newpublickey = parsed.exportKey("components-public")
-
-//const publicKey2=getKey('gmail.com','20221208');
-
-//console.log(publicKey2);
-
